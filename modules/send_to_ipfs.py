4369651a11b967a7d0cb57c5072aa8f084a428e7
PaTara43 <p040399@outlook.com>
2020-10-29 20:34:28 +0300

    added pinning to pinata, deleted pinning to temporal

--- modules/send_to_ipfs.py
+++ modules/send_to_ipfs.py
@@ -5,46 +5,42 @@ import os
 import requests
 import subprocess
 
+from pinatapy import PinataPy
+
 
 def send(cam, config):
     if config['ipfs']['enable']:
         try:
-            logging.warning('Pushing to IPFS')
+            logging.warning("Camera " + cam.camera_name + ' Pushing to IPFS')
             client = ipfshttpclient.connect()
             res = client.add(cam.filename)
-            logging.warning('Pushed, hash: ' + res['Hash'])
-            _pin_to_temporal(config, cam.filename)
+            logging.warning("Camera " + cam.camera_name + ' Pushed, hash: ' + res['Hash'])
+            _pin_to_pinata(config, cam.filename)
         except Exception as e:
-            logging.error("Error while pushing to IPFS, error: ", e)
+            logging.error("Camera " + cam.camera_name + " Error while pushing to IPFS, error: ", e)
 
     if config['general']['delete_after_record']:
         try:
-            logging.warning('Removing file')
+            logging.warning("Camera " + cam.camera_name + ' Removing file')
 
             os.remove(cam.filename)
         except Exception as e:
-            logging.error("Error while deleteng file, error: ", e)
+            logging.error("Camera " + cam.camera_name + " Error while deleteng file, error: ", e)
 
     if config['datalog']['enable']:
         try:
             program = "echo \"" + res['Hash'] + "\" | " + config['transaction']['path_to_robonomics_file'] + "robonomics io write datalog -s " + cam.key
             process = subprocess.Popen(program, shell=True, stdout=subprocess.PIPE)
             output = process.stdout.readline()
-            logging.warning("Published data to chain. Transaction hash is " + output.strip().decode('utf8'))
+            logging.warning("Camera " + cam.camera_name + " Published data to chain. Transaction hash is " + output.strip().decode('utf8'))
         except Exception as e:
-            logging.error("Error while sending IPFS hash to chain, error: ", e)
+            logging.error("Camera " + cam.camera_name + " Error while sending IPFS hash to chain, error: ", e)
 
-def _pin_to_temporal(config, file_path: str):
-        username = config["temporal"]["temporal_username"]
-        password = config["temporal"]["temporal_password"]
-        if username and password:
-            auth_url = "https://api.temporal.cloud/v2/auth/login"
-            token_resp = requests.post(auth_url, json={"username": username, "password": password})
-            token = token_resp.json()
 
-            url_add = "https://api.temporal.cloud/v2/ipfs/public/file/add"
-            headers = {"Authorization": f"Bearer {token['token']}"}
-            resp = requests.post(url_add, files={"file":file_path, "hold_time":(None,'1')}, headers=headers)
-
-            if resp.status_code == 200:
-                logging.warning("File pinned to Temporal Cloud")
+def _pin_to_pinata(config, file_path: str):
+        pinata_api = config["pinata"]["pinata_api"]
+        pinata_secret_api = config["pinata"]["pinata_secret_api"]
+        if pinata_api and pinata_secret_api:
+            pinata = PinataPy(pinata_api, pinata_secret_api)
+            pinata.pin_file_to_ipfs(file_path)
+            logging.warning("Camera " + cam.camera_name + " Pinata hash" + pinata.pin_list()['rows'][0]['ipfs_pin_hash'])
