54a3747ec9d3208d4b6d8e57936db4ee28f4953b
PaTara43 <p040399@outlook.com>
2020-10-29 20:51:24 +0300

    pinata corrections 2

--- modules/send_to_ipfs.py
+++ modules/send_to_ipfs.py
@@ -9,20 +9,13 @@ from pinatapy import PinataPy
 
 
 def send(cam, config):
+    logging.warning("Camera " + cam.camera_name + ' sending file to pinata')
+
     if config['pinata']['enable']:
         try:
-            _pin_to_pinata(config, cam.filename)
+            hash = _pin_to_pinata(cam, config)
         except Exception as e:
-            logging.error("Camera " + cam.camera_name + " Error while pinning gto pinata, error: ", e)
-
-    if config['ipfs']['enable']:
-        try:
-            logging.warning("Camera " + cam.camera_name + ' Pushing to IPFS')
-            client = ipfshttpclient.connect()
-            res = client.add(cam.filename)
-            logging.warning("Camera " + cam.camera_name + ' Pushed, hash: ' + res['Hash'])
-        except Exception as e:
-            logging.error("Camera " + cam.camera_name + " Error while pushing to IPFS, error: ", e)
+            logging.error("Camera " + cam.camera_name + " Error while pinning to pinata, error: ", e)
 
     if config['general']['delete_after_record']:
         try:
@@ -34,7 +27,7 @@ def send(cam, config):
 
     if config['datalog']['enable']:
         try:
-            program = "echo \"" + res['Hash'] + "\" | " + config['transaction']['path_to_robonomics_file'] + "robonomics io write datalog -s " + cam.key
+            program = "echo \"" + hash + "\" | " + config['transaction']['path_to_robonomics_file'] + "robonomics io write datalog -s " + cam.key
             process = subprocess.Popen(program, shell=True, stdout=subprocess.PIPE)
             output = process.stdout.readline()
             logging.warning("Camera " + cam.camera_name + " Published data to chain. Transaction hash is " + output.strip().decode('utf8'))
@@ -42,10 +35,11 @@ def send(cam, config):
             logging.error("Camera " + cam.camera_name + " Error while sending IPFS hash to chain, error: ", e)
 
 
-def _pin_to_pinata(config, file_path: str):
+def _pin_to_pinata(cam, config):
         pinata_api = config["pinata"]["pinata_api"]
         pinata_secret_api = config["pinata"]["pinata_secret_api"]
         if pinata_api and pinata_secret_api:
             pinata = PinataPy(pinata_api, pinata_secret_api)
-            pinata.pin_file_to_ipfs(file_path)
-            logging.warning("Camera " + cam.camera_name + " Pinata hash" + pinata.pin_list()['rows'][0]['ipfs_pin_hash'])
+            pinata.pin_file_to_ipfs(cam.filename)
+            logging.warning("Camera " + cam.camera_name + " Pinata hash is " + pinata.pin_list()['rows'][0]['ipfs_pin_hash'])
+            return pinata.pin_list()['rows'][0]['ipfs_pin_hash']
