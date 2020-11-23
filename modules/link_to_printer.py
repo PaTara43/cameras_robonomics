c6f4588c3b65462ca94d01ed552f8c2205758963
PaTara43 <p040399@outlook.com>
2020-11-23 19:28:00 +0300

    Corrections printing qr-code

--- modules/link_to_printer.py
+++ modules/link_to_printer.py
@@ -2,18 +2,18 @@ import cups
 import logging
 import time
 
-
 class Task(self):
 
     def __init__(self):
         logging.warning("Initializing printer")
         self.conn = cups.Connection()
         self.printers = self.conn.getPrinters()
-        self.printer_name =self. printers.keys()[0]
+        self.printer_name =list(self.printers.keys())[0]
 
     def send_task_to_printer(self, picname):
         logging.warning("Printing...")
-        self.print_id = self.conn.printFile(self.printer_name, picname, "QR-code", {})
+        self.print_id = self.conn.printFile(self.printer_name, picname, "QR-code",{'PaperType':'LabelGaps',\
+        'GapsHeight':'2', 'collate':'true', 'media':'Custom.39x39mm'})
         # Wait until the job finishes
         while self.conn.getJobs().get(self.print_id, None):
             time.sleep(1)
