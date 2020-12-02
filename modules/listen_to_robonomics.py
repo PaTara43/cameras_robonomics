d37ac8c6cb2275fcbbf262a0ac3399568e32bd36
PaTara43 <p040399@outlook.com>
2020-12-02 18:23:52 +0300

    process stop transaction

--- modules/listen_to_robonomics.py
+++ modules/listen_to_robonomics.py
@@ -28,28 +28,28 @@ def listener(config, cam, dirname):
                     logging.warning("Camera is busy. Record aborted")
                     continue
                 cam.is_busy = True
+                cam.stop_record = False
                 start_record_cam_thread = Thread(target=start_record_cam, args=(cam,))
                 start_record_cam_thread.start()
                 create_url_r_thread = Thread(target=create_url_r, args=(cam, dirname,))
                 create_url_r_thread.start()
             elif ('>> ' + config['camera']['address'] + " : false") in output.strip().decode('utf-8'):
-                if not cam.is_busy:
-                    logging.warning("Camera is not recording yet. Nothing to stop")
-                    continue
                 logging.warning('Transaction to stop recording')
+                if not cam.is_busy:
+                    logging.warning("Camera is not recording. Nothing to stop")
+                    continue
+                cam.stop_record = True
+                cam.is_busy = False
                 stop_record_cam_thread = Thread(target=stop_record_cam, args=(cam.filename, cam.keyword, cam.qrpic, config,))
                 stop_record_cam_thread.start()
 
 
 
 def start_record_cam(cam):
-    cam.stop_record = False
     cam.record()
 
 
 def stop_record_cam(filename, keyword, qrpic, config):
-    cam.stop_record = True
-    cam.is_busy = False
     time.sleep(1)
     send(filename, keyword, qrpic, config)
 
